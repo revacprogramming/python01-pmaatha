763bc6cbe117fda6aaa0f854ff337b3cbf0060d3
Mamatha P <97168002+pmaatha@users.noreply.github.com>
2022-05-30 04:06:48 +0000

    complited

--- ActivitySet01/problem04.py
+++ ActivitySet01/problem04.py
@@ -1,3 +1,13 @@
 # Conditional Execution
 
-hrs = input("Enter hours? ")
+hrs = input("Enter hours: ")
+rate = input("enter rate: ")
+fhrs=float(hrs)
+frate=float(rate)
+if fhrs>40:
+  regularpay=40*frate
+  overtimepay=(fhrs-40)*(frate*1.5)
+  Pay=regularpay+overtimepay
+else:
+  Pay=fhrs*frate
+  print("Pay",Pay)typr
